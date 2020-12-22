67d21cc51782f8f997f7af6d31187c67c2246c69
DonTron-prog <75101968+DonTron-prog@users.noreply.github.com>
2020-12-21 19:55:40 -0500

    Update settings.py

--- src/clawfoot/settings.py
+++ src/clawfoot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+-=qflcs4swdb%3+ci4w)tx_!#skqghq37%neeuitp$gsp_wv)'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
