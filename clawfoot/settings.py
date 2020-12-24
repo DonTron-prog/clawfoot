c08578edc11d742bf19ec4894d294eea474d5414
DonTron-prog <mcgillivray.d@gmail.com>
2020-12-24 02:41:56 +0000

    remove secret key

--- clawfoot/settings.py
+++ clawfoot/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+-=qflcs4swdb%3+ci4w)tx_!#skqghq37%neeuitp$gsp_wv)'
+with open(BASE_DIR.parent.parent / 'claw_secretkey.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
